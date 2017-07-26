b85c3cc58093b14a7e2f843e9989dbba16ae50ed
Jay Welborn <jesse.welborn@gmail.com>
2017-07-26 21:14:54 +0200

    parallaxed header, refined responsiveness

--- js/juan.js
+++ js/juan.js
@@ -1,6 +1,16 @@
+function projectParallax(){
+    var scrolled = $(window).scrollTop();
+    if(scrolled < $('#myCarousel').offset().top + $(window).height()) {
+        $('#myCarousel').css('top', (0+(scrolled * 0.3)) + 'px');
+    }
+}
+    
+
 $(window).scroll(function(){
 
     var windowScroll = $(this).scrollTop();
+
+    projectParallax();
     
     if ($('.featurette:first').offset().top - $(window).height() * (1/1.1618) < windowScroll)  {
         $('.featurette:first').addClass('is-showing');
